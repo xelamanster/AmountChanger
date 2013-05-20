e55dbf7d6bd7c895b8d842dccc655223ae167cbc
xelamanster <xelamanster@gmail.com>
2013-05-20 03:26:00 +0300

    Final version

--- gen/com/example/newamountchanger/R.java
+++ gen/com/example/newamountchanger/R.java
@@ -21,11 +21,10 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int button_icon=0x7f020000;
-        public static final int buttonoff=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int over=0x7f020003;
-        public static final int shape10=0x7f020004;
+        public static final int backgrnd=0x7f020000;
+        public static final int button=0x7f020001;
+        public static final int button_icon=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
         public static final int action_settings=0x7f080005;
@@ -45,10 +44,11 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
-        public static final int addBtn=0x7f050004;
+        public static final int addBtn=0x7f050005;
         public static final int amount=0x7f050003;
+        public static final int amountDialog=0x7f050004;
         public static final int app_name=0x7f050000;
-        public static final int dialogBtn=0x7f050005;
+        public static final int dialogBtn=0x7f050006;
         public static final int title_activity_dialog=0x7f050002;
     }
     public static final class style {
--- src/com/example/newamountchanger/MainActivity.java
+++ src/com/example/newamountchanger/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-		
 		amnt=amountEdit.getText().toString();
 		if(data!=null){
 			String newAmount = data.getStringExtra("amount");
